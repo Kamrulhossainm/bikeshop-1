5f5fce425d35e03fe9d0aaf8d4d59de19c1d78a7
kamrul <mkamrulh456@gmail.com>
2025-09-19 07:21:02 +0600

    quiz swipe mechanism

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,6 +1,9 @@
 const finish_btn = document.getElementById("finish-button");
-
 const quiz_form = document.querySelector("#quizForm");
+const prevButton = document.querySelector('#prev-button');
+const nextButton = document.querySelector('#next-button')
+
+
 const answers = {};
 
 quiz_form.addEventListener("change", updateAnswers);
@@ -14,6 +17,54 @@ function updateAnswers(event) {
   console.log("answers: ", answers);
 }
 
+let currentQuestionIndex = 0;
+
+
+// Next Button
+nextButton.addEventListener("click", function(){
+  currentQuestionIndex++;
+  displayQuestion(currentQuestionIndex)
+  updateButtonStates(currentQuestionIndex)
+})
+
+
+// previous button
+prevButton.addEventListener("click", function(){
+  currentQuestionIndex--;
+  displayQuestion(currentQuestionIndex)
+  updateButtonStates(currentQuestionIndex)
+})
+
+function displayQuestion(index){
+  let current_question = questions[index]
+  let selected_answer = answers[current_question.name] || '';
+  let question_html = `
+    <h2>${current_question.text}</h2>
+     <div class="label-container">
+    ${current_question.options.map(option => `
+   
+      <input ${option.value === selected_answer ? "checked": ""} type="radio" id="${option.value}" name="${current_question.name}" value="${option.value}">
+      <label for="${option.value}">
+        <div class="img-wrap">
+          <img src="${option.img}">
+        </div>
+        <span>${option.label}</span>
+      </label>`
+    ).join('')}
+  </div>
+  `
+  
+  quiz_form.innerHTML = question_html
+}
+
+
+function updateButtonStates (index){
+  prevButton.disabled = (index === 0);
+  nextButton.classList.toggle("hidden", index === questions.length -1);
+  finish_btn.classList.toggle("hidden", index != questions.length - 1);
+}
+ updateButtonStates(currentQuestionIndex);
+displayQuestion(currentQuestionIndex)
 
 finish_btn.addEventListener("click", function () {
   if (answers.product_type && answers.bike_type && answers.color) {
